3d9151ea5c6cc81521279123bf95817e0e17632a
Karthik Chandrasekar <karthidec1@gmail.com>
2013-10-14 19:54:12 -0700

    Code to generate anonymized twitter data is added

--- AnonymizeDataSet.py
+++ AnonymizeDataSet.py
@@ -8,16 +8,21 @@ class AnonymizeData:
 
 		#Filenames
 		self.friends_info_file = os.path.join(self.output_dir,'crawled_nodes_output')
-		self.anonymized_twitter_data_file = os.path.join(self.output_dir, 'UserIdMap')
+		self.twitter_data_userid_hash_file = os.path.join(self.output_dir, 'UserIdMap')
+		self.anonymized_twitter_data_file = os.path.join(self.output_dir, 'AnonymizedTwitterDataSet')
+
 
 		#FileDescriptor
 		self.friends_info_fd = open(self.friends_info_file, 'r')
+		self.twitter_data_userid_hash_fd = codecs.open(self.twitter_data_userid_hash_file, 'w', 'utf-8')
 		self.anonymized_twitter_data_fd = codecs.open(self.anonymized_twitter_data_file, 'w', 'utf-8')
-		
+
 		#DataStructures
 		self.uniq_node_set = set()
 		self.uniq_node_list = []
 		self.old_id_to_new_id_dict = {}
+		self.anonymized_twitter_data = {}
+
 
 	def run_main(self):
 		self.run()
@@ -35,6 +40,12 @@ class AnonymizeData:
 		#Dump new_id to old_id hash
 		self.dump_old_id_to_new_id_dict()
 	
+		#Generate Anonymized Twitter data
+		self.anonymize_twitter_data()
+
+		#Dump anonymized twitter data
+		self.dump_anonymize_twitter_data()
+
 
 	def get_uniq_nodes(self):
 		for key, value in self.friends_info_dict.iteritems():
@@ -50,14 +61,26 @@ class AnonymizeData:
 		self.uniq_node_list.sort()
 
 		for new_id, old_id in enumerate(self.uniq_node_list):
-			self.old_id_to_new_id_dict[old_id] = [new_id]
+			self.old_id_to_new_id_dict[old_id] = new_id
 			
 
 	def dump_old_id_to_new_id_dict(self):
-		self.anonymized_twitter_data_fd.write(json.dumps(self.old_id_to_new_id_dict))
+		self.twitter_data_userid_hash_fd.write(json.dumps(self.old_id_to_new_id_dict))
 		
+		self.twitter_data_userid_hash_fd.close()
+
+	def anonymize_twitter_data(self):
+		for key, value in self.friends_info_dict.iteritems():
+			new_key_id = self.old_id_to_new_id_dict.get(key)
+			for node in value:
+				new_value_id = self.old_id_to_new_id_dict.get(node)
+				self.anonymized_twitter_data.setdefault(new_key_id, []).append(new_value_id)
+
+
+	def dump_anonymize_twitter_data(self):
+		self.anonymized_twitter_data_fd.write(json.dumps(self.anonymized_twitter_data))
+
 		self.anonymized_twitter_data_fd.close()
-	
 
 
 if __name__ == "__main__":
